e1ba15881f29a762460dee81605b30937a9ad646
Lewis <lewis.evans@uk.ibm.com>
2020-12-06 20:51:56 +0000

    Here goes ...

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # todos
 Can I display my todo list on the pi?
+
+A very simple program designed to show my todos on my Pi using the unicorn-hat-hd matrix screen
---
+++ todo.py
@@ -0,0 +1,66 @@
+import unicornhathd
+import time
+
+try:
+    from PIL import Image, ImageDraw, ImageFont
+except ImportError:
+    exit('This script requires the pillow module\nInstall with: sudo pip install pillow')
+
+# Get the width and height of the display
+width, height = unicornhathd.get_shape()
+
+# Select font and size
+FONT = ('/usr/share/fonts/truetype/freefont/FreeSansBold.ttf', 10)
+
+
+def display(todo):
+    print(todo)
+
+    # Set the rotation of the display
+    unicornhathd.rotation(90)
+
+    # We want to draw our text 1 pixel in, and 2 pixels down from the top left corner
+    text_x = 1
+    text_y = 2
+
+    # Grab our font file and size as defined at the top of the script
+    font_file, font_size = FONT
+
+    # Load the font using PIL's ImageFont
+    font = ImageFont.truetype(font_file, font_size)
+
+    # Ask the loaded font how big our text will be
+    text_width, text_height = font.getsize(todo)
+
+    # Make sure we accommodate enough width to account for our text_x left offset
+    text_width += width + text_x
+
+    # Now let's create a blank canvas wide enough to accomodate our text
+    image = Image.new('RGB', (text_width, max(height, text_height)), (0, 0, 0))
+
+    # To draw on our image, we must use PIL's ImageDraw
+    draw = ImageDraw.Draw(image)
+
+    # And now we can draw text at our desited (text_x, text_y) offset, using our loaded font
+    draw.text((text_x, text_y), todo, fill=(255, 255, 255), font=font)
+
+    for scroll in range(text_width - width):
+        for x in range(width):
+            for y in range(height):
+                pixel = image.getpixel((x + scroll, y))
+                r, g, b = [int(n) for n in pixel]
+                unicornhathd.set_pixel(width - 1 - x, r, g, b)
+            unicornhathd.show()
+            time.sleep(0.01)
+
+
+def readTodos():
+    todoFile = open('todos.txt')
+    todoLines = todoFile.readlines()
+    return todoLines
+
+
+todos = readTodos()
+
+for todo in todos:
+    display(todo.strip())
